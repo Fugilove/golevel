625c0bbede60dd76e43262533b6bbd1c3d952b3e
Fugilove <albertmarkov8173@gmail.com>
2024-07-23 14:53:17 +0400

    Обновление кода для обработки статических файлов

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"golevel/config"
 	"golevel/routes"
+	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -12,7 +13,15 @@ func main() {
 	config.LoadConfig()
 
 	r := mux.NewRouter()
+
+	// Обработка статических файлов
+	r.PathPrefix("/css/").Handler(http.StripPrefix("/css/", http.FileServer(http.Dir("public/css"))))
+	r.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("public/images"))))
+
+	// Регистрация маршрутов
 	routes.RegisterRoutes(r)
+
 	http.Handle("/", r)
-	http.ListenAndServe(":8080", nil)
+	log.Println("Server started at :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
