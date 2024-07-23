329b671396e327eddf6cc5e9fa0892a741356b46
Fugilove <albertmarkov8173@gmail.com>
2024-07-23 14:17:12 +0400

    Initial commit with basic structure of GoLevel

---
+++ install.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: golevel new <project_name>")
+		return
+	}
+
+	command := os.Args[1]
+	projectName := os.Args[2]
+
+	if command == "new" && projectName != "" {
+		createProject(projectName)
+	} else {
+		fmt.Println("Invalid command or project name")
+	}
+}
+
+func createProject(projectName string) {
+	fmt.Println("Creating project:", projectName)
+
+	// Clone the GoLevel repository
+	cmd := exec.Command("git", "clone", "https://github.com/yourusername/golevel", projectName)
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error cloning repository:", err)
+		return
+	}
+
+	// Change directory to the project
+	err = os.Chdir(projectName)
+	if err != nil {
+		fmt.Println("Error changing directory:", err)
+		return
+	}
+
+	// Initialize Go module
+	cmd = exec.Command("go", "mod", "init", projectName)
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error initializing Go module:", err)
+		return
+	}
+
+	// Install dependencies
+	cmd = exec.Command("go", "mod", "tidy")
+	err = cmd.Run()
+	if err != nil {
+		fmt.Println("Error installing dependencies:", err)
+		return
+	}
+
+	fmt.Println("Project", projectName, "created successfully!")
+}
