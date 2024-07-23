5096dc9d37f8cfd60bfe60ac80ca2846fbaaf97f
Fugilove <albertmarkov8173@gmail.com>
2024-07-23 14:34:50 +0400

    Initial commit with basic structure of GoLevel

--- install.go
+++ install.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"runtime"
+	"strings"
 )
 
 func main() {
@@ -16,45 +19,88 @@ func main() {
 	projectName := os.Args[2]
 
 	if command == "new" && projectName != "" {
-		createProject(projectName)
+		err := createProject(projectName)
+		if err != nil {
+			fmt.Println("Error creating project:", err)
+		}
+	} else if command == "install" {
+		err := install()
+		if err != nil {
+			fmt.Println("Error installing GoLevel:", err)
+		}
 	} else {
 		fmt.Println("Invalid command or project name")
 	}
 }
 
-func createProject(projectName string) {
+func createProject(projectName string) error {
 	fmt.Println("Creating project:", projectName)
 
 	// Clone the GoLevel repository
-	cmd := exec.Command("git", "clone", "https://github.com/Fugilove/golevel", projectName)
+	cmd := exec.Command("git", "clone", "https://github.com/yourusername/golevel", projectName)
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("Error cloning repository:", err)
-		return
+		return fmt.Errorf("error cloning repository: %w", err)
 	}
 
 	// Change directory to the project
 	err = os.Chdir(projectName)
 	if err != nil {
-		fmt.Println("Error changing directory:", err)
-		return
+		return fmt.Errorf("error changing directory: %w", err)
 	}
 
 	// Initialize Go module
 	cmd = exec.Command("go", "mod", "init", projectName)
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println("Error initializing Go module:", err)
-		return
+		return fmt.Errorf("error initializing Go module: %w", err)
 	}
 
 	// Install dependencies
 	cmd = exec.Command("go", "mod", "tidy")
 	err = cmd.Run()
 	if err != nil {
-		fmt.Println("Error installing dependencies:", err)
-		return
+		return fmt.Errorf("error installing dependencies: %w", err)
 	}
 
 	fmt.Println("Project", projectName, "created successfully!")
+	return nil
+}
+
+func install() error {
+	if runtime.GOOS != "windows" {
+		return fmt.Errorf("install script only supports Windows")
+	}
+
+	fmt.Println("Installing GoLevel...")
+
+	// Compile the binary
+	cmd := exec.Command("go", "build", "-o", "golevel", "install.go")
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("error building binary: %w", err)
+	}
+
+	// Get the path of the current executable
+	executablePath, err := os.Executable()
+	if err != nil {
+		return fmt.Errorf("error getting executable path: %w", err)
+	}
+	dirPath := filepath.Dir(executablePath)
+
+	// Determine the PATH environment variable
+	pathEnv := os.Getenv("PATH")
+	if !strings.Contains(pathEnv, dirPath) {
+		fmt.Println("Adding GoLevel to PATH...")
+
+		// Add the directory to PATH
+		newPath := fmt.Sprintf("%s;%s", dirPath, pathEnv)
+		err = os.Setenv("PATH", newPath)
+		if err != nil {
+			return fmt.Errorf("error setting PATH environment variable: %w", err)
+		}
+	}
+
+	fmt.Println("GoLevel installed successfully!")
+	return nil
 }
